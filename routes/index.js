1a1785a911e022011a705ec54eb15ac05d226097
Arturo Ocampo <arturoocampo@byui.edu>
2025-09-15 19:50:35 +0100

    Week 1 - Sarah Birch (Updated 2)

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
+// Rutas principales de la aplicación
 const routes = require('express').Router();
-
 const myController = require('../controllers');
 
+// Definimos las rutas y las funciones del controlador que manejarán las solicitudes
 routes.get('/', myController.awesomeFunction);
 routes.get('/awesome', myController.returnAnotherPerson);
 
+// Exportamos las rutas para usarlas en el servidor principal
 module.exports = routes;
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+// Estamos creando un servidor básico con Express (Libreria mas usada para crear servidores en Node.js)
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
+//Maneja las peticiones que llegan a la raíz
 app.use('/',require('./routes'));
 
+//El servidor esta escuchando en este puerto
 app.listen(port,() => {
     console.log('Server is running on port ${port}');
 })
